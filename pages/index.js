f59b0421beaa5591c85af3f0abc3e0f05c9b8a79
Shayan Jafari <shayanja12345@gmail.com>
2023-07-01 14:04:48 +0330

    test graphql in project

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
-export default function Home() {
+import { gql } from "@apollo/client";
+import client from "client";
+
+export default function Home(props) {
+  console.log(props);
   return <div>Next JS &amp; WordPress course.</div>;
 }
+
+export const getStaticProps = async () => {
+  const {data}  = await client.query({
+    query: gql`
+      query NewQuery {
+        pages {
+          nodes {
+            title
+          }
+        }
+      }
+    `,
+  });
+  return {
+    props: {data},
+  };
+};
