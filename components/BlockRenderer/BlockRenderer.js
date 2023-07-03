3356fd8b72e9184347f5fd7d69ac59cd0114b858
Shayan Jafari <shayanja12345@gmail.com>
2023-07-03 14:15:41 +0330

    complete style Heading components

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,11 +1,29 @@
 import { Cover } from "components/Cover";
+import { Heading } from "components/Heading";
 
 export const BlockRenderer = ({ blocks }) => {
   return blocks.map((block) => {
     switch (block.name) {
-      case "core/cover": {
-        return <Cover key={block.id} background={block.attributes.url}>core cover</Cover>;
+      case "core/heading": {
+        console.log(block);
+        return (
+          <Heading
+            key={block.id}
+            textAlign={block.attributes.textAlign}
+            level={block.attributes.level}
+            content={block.attributes.content}
+          />
+        );
       }
+      case "core/cover": {
+        console.log(block);
+        return (
+          <Cover key={block.id} background={block.attributes.url}>
+            <BlockRenderer blocks={block.innerBlocks} />
+          </Cover>
+        );
+      }
+
       default:
         return null;
     }
--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export const Cover = ({ children, background }) => {
   return (
-    <div className="h-screen bg-slate-800 relative min-h-[400px] flex justify-center items-center">
+    <div className="h-screen text-white bg-slate-800 relative min-h-[400px] flex justify-center items-center">
       <Image
         alt="Cover"
         src={background}
         fill
         className="mix-blend-soft-light object-cover"
       />
-      {children}
+      <div className="max-w-5xl z-10">{children}</div>
     </div>
   );
 };
