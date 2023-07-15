1e9766178a11635f565d6178d65d6ae0ea0de678
Shayan Jafari <shayanja12345@gmail.com>
2023-07-15 14:05:32 +0330

    fix issues hyperlink to localhost

--- components/Paragraph/Paragraph.js
+++ components/Paragraph/Paragraph.js
@@ -1,11 +1,12 @@
 import { getTextAlign } from "utils/fonts";
+import { relativeToAbsoluteUrls } from "utils/relativeToAbsoluteUrls";
 
 export const Paragraph = ({ content, textAlign = "left", textColor }) => {
   return (
     <p
       className={`max-w-5xl mx-auto ${getTextAlign(textAlign)}`}
       style={{ color: textColor }}
-      dangerouslySetInnerHTML={{ __html: content }}
+      dangerouslySetInnerHTML={{ __html: relativeToAbsoluteUrls(content) }}
     />
   );
 };
