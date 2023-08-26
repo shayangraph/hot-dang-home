e4b2556cffcd18dce3c7608e5ed4fe7ce7a1a82c
Shayan Jafari <shayanja12345@gmail.com>
2023-08-26 16:33:42 +0330

    fix data search issues

--- components/PropertySearch/PropertySearch.js
+++ components/PropertySearch/PropertySearch.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export const PropertySearch = () => {
   useEffect(() => {
     const search = async () => {
-      const res = await fetch(`/api/search`);
+      const res = await fetch('/api/search');
       const data = await res.json();
       console.log("DATA SEARCH", data);
     };
