a2d7b129ccbbf76f5762cb94dc81313148069032
Shayan Jafari <shayanja12345@gmail.com>
2023-09-20 17:39:25 +0330

    fix tickitem style

--- components/TickItem/TickItem.js
+++ components/TickItem/TickItem.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const TickItem = ({ children }) => {
   return (
-    <div className="grid grid-cols-[50px_1fr] gap-3">
+    <div className="grid grid-cols-[50px_1fr] gap-3 pb-10">
       <div className="text-3xl text-green-500 justify-center items-center">
         <FontAwesomeIcon icon={faCircleCheck} />
       </div>
